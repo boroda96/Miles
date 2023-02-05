4715cb1b9df73c84a50070bb10a70c11f2b58e98
Kurtsev Dmitriy <boman@bk.ru>
2023-02-05 10:55:37 +0500

    reformat code

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,17 @@
 public class Main {
     public static void main(String[] args) {
 
-        // Объявляете переменные для входных данных и
-        // параметров программы: одну для хранения
-        // стоимости билета, другую для хранения количества
-        // рублей для одной бонусной мили
         System.out.println("Добрый день!");
         int amountTicket = 1000;
         System.out.println("Стоимость билета " + amountTicket + " руб.");
         System.out.println("У нас работает бонусная программа, за каждые 20 рублей стоимости билета" +
                 " вам будет начисляться 1 бесплатная бонусная миля.");
         int amountOneMiles = 20;
-        int bonusMiles = amountTicket/amountOneMiles;
+        int bonusMiles = amountTicket / amountOneMiles;
 
 
         System.out.println("Ваш бонус составляет " + bonusMiles + " миль.");
 
-        // Рассчитываете количество бонусных миль, используя
-        // значения заведённых переменных. Ответ сохраняете в
-        // новую переменную и выводите на экран
-
+        
     }
 }
